04c1dabf2355d437aa8f3f2ff654d8db9534a3ca
Ali Mehrabian <69728416+Ali-Meh619@users.noreply.github.com>
2025-10-08 20:08:14 -0700

    NEW EXP

--- assets/js/main.js
+++ assets/js/main.js
@@ -255,6 +255,15 @@
   });
 
   /**
+   * Timeline animation enhancement
+   */
+  document.addEventListener('aos:in', ({ detail }) => {
+    if (detail && detail.classList.contains('timeline-item')) {
+      detail.style.opacity = '1';
+    }
+  });
+
+  /**
    * Initiate Pure Counter 
    */
   new PureCounter();
